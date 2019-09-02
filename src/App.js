4d005e85eeea1c42bd435f8038c44de6d28b3cef
northerchild <osarus-13@hotmail.com>
2019-09-01 19:22:33 -0500

    Se leen los datos que pasa el usuario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './bootstrap.min.css';
 import Header from './components/Header';
 import NuevaCita from './components/NuevaCita';
@@ -7,15 +6,20 @@ import NuevaCita from './components/NuevaCita';
 class App extends Component {
     state = {}
     render() {
-        return ( 
-            <div className = "container" >
-            <Header titulo = 'Administrador Pacientes Veterinaria' / >
-                <div className = "row" >
-                    <div className = "col-md-10 mx-auto" >
-                    <NuevaCita / >
-                    </div> 
-                </div> 
-            </div>
+        return ( <
+            div className = "container" >
+            <
+            Header titulo = 'Administrador Pacientes Veterinaria' / >
+            <
+            div className = "row" >
+            <
+            div className = "col-md-10 mx-auto" >
+            <
+            NuevaCita / >
+            <
+            /div>  <
+            /div>  <
+            /div>
         );
     }
 }
--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class NuevaCita extends Component {
-    state = {}
+    state = {
+        cita:{
+            mascota:'',
+            propietario:'',
+            fecha:'',
+            hora:'',
+            sintomas:''
+        }
+    }
+    handleChange= e=>{
+        console.log(e.target.name + ":" + e.target.value);
+        //Colocar lo que el usuario escribe en el state
+        this.setState({
+            cita: {
+                ...this.state.cita,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
     render() {
         return ( 
             <div className = "card mt-5 py-5" >
@@ -14,8 +32,10 @@ class NuevaCita extends Component {
             <input type = "text"
             className = "form-control"
             placeholder = "Nombre Mascota"
-            name = "mascota" /
-            >
+            name = "mascota"
+            onChange={this.handleChange}
+            value = {this.state.cita.mascota}
+            />
             </div> 
             </div > { /*form - group*/ } 
 
@@ -25,8 +45,10 @@ class NuevaCita extends Component {
             <input type = "text"
             className = "form-control"
             placeholder = "Nombre Propietario"
-            name = "propietario" /
-            >
+            name = "propietario" 
+            onChange={this.handleChange}
+            value = {this.state.cita.propietario}
+            />
             </div> 
             </div > { /*form - group*/ } 
 
@@ -35,32 +57,33 @@ class NuevaCita extends Component {
             <div className="col-sm-8 col-lg-4">
             <input type = "date"
             className = "form-control"
-            name = "fecha" /
-            >
+            name = "fecha"
+            onChange={this.handleChange}
+            value = {this.state.cita.fecha}    
+             />
             </div> 
             <label className = "col-sm-4 col-lg-2 col-form-label" > Hora < /label> 
             <div className="col-sm-8 col-lg-4">
             <input type = "time"
             className = "form-control"
-            name = "hora" /
-            >
-            </div> 
-            </div > { /*form - group*/ } 
-
-            <div className = "form-group row" >
-            <label className = "col-sm-4 col-lg-2 col-form-label" > Fecha < /label> 
-            <div className="col-sm-8 col-lg-4">
-            <input type = "date"
-            className = "form-control"
-            name = "fecha" /
-            >
+            name = "hora" 
+            onChange={this.handleChange}
+            value = {this.state.cita.hora}    
+            />
             </div> 
             </div > { /*form - group*/ } 
 
             <div className = "form-group row" >
             <label className = "col-sm-4 col-lg-2 col-form-label" > Sintomás < /label> 
             <div className="col-sm-8 col-lg-10">
-            <textarea name="sintomas" className="form-control" placeholder="describe los sintomás"></textarea>
+            <textarea 
+            name="sintomas" 
+            className="form-control" 
+            placeholder="describe los sintomás"
+            onChange={this.handleChange}
+            value = {this.state.cita.mascota}
+            >
+            </textarea>
             </div> 
             </div > { /*form - group*/ } 
 
